6de63cd7551b7c2f4c078e244cc3f251276cebc2
si00yun <sipu0641@uni.sydney.edu.au>
2025-11-08 08:31:35 +0800

    draw circle

--- sketch.js
+++ sketch.js
@@ -1,8 +1,111 @@
+
+
+let bg;                //存放背景颜色
+let colorSet;          //存放所有颜色
+let rings = [];       // 保存每个圆的随机参数（位置/半径/配色）
+
 function setup() {
-  createCanvas(400, 400);
+  //Create a canvas as large as the current browser window
+  createCanvas(windowWidth, windowHeight);
+
+  colorSet = [
+    color(10,13,24),     // bg
+    color(255, 90, 0),   // hot orange
+    color(255, 0, 110),  // magenta
+    color(80, 220, 100), // mint
+    color(255, 200, 0),  // yellow
+    color(0, 210, 255),  // cyan
+    color(140,110,255),  // violet
+    color(255, 80,170),  // pink
+    color(255,120, 40),  // orange2
+    color(40, 255,200)   // aqua
+  ];
+  bg = colorSet[0];
+  
+  generateLayout(); 
 }
 
 function draw() {
-  background(220);
-  111111
+  background(bg);
+   for (let ring of rings){
+    drawCircle(ring);    // 绘制每一个“同样形状”的圆
+  }
+}
+
+// ===== 生成圆的数据（位置/半径/配色） =====
+function generateLayout(){
+  rings = [];
+  let attempts = 0; // 建立一个计数器，防止无限循环
+  let target = 10;  // 在页面中画10个圆
+  while (rings.length < target && attempts < 2000){
+    attempts++;
+
+    // 控制半径范围
+    let Rmin = min(width,height) * 0.05;   
+    let Rmax = min(width,height) * 0.12;   
+    let r = random(Rmin, Rmax);
+
+    // 随机位置
+    let x = random(r + 20, width  - r - 20);
+    let y = random(r + 20, height - r - 20);
+
+    // 为这个圆选 5 个配色
+    let palette = [
+      random(colorSet),   // 外环颜色
+      random(colorSet),   // 辐条颜色
+      random(colorSet),   // 中环颜色
+      random(colorSet),   // 点阵颜色
+      random(colorSet)    // 中心帽颜色
+    ];
+
+    // 把这个圆的数据存进数组
+    rings.push({ x, y, r,palette });
+  }
+}
+
+// ===== 绘制圆（外环/辐条/中环/点阵/中心帽） =====
+function drawOne(ring){
+  // 外粗环
+  strokeWeight(max(2, ring.r * 0.06));
+  stroke(ring.palette[0]);
+  noFill();
+  circle(ring.x, ring.y, ring.r * 2);
+
+  // 放射线条
+  let nSpokes = 20;  //线条数量
+  strokeWeight(2);
+  stroke(ring.palette[1]);
+
+  for (let i = 0; i < nSpokes; i++){
+    let ang = i * TWO_PI / nSpokes; 
+    let x1 = ring.x + ring.r * 0.12 * cos(ang);
+    let y1 = ring.y + ring.r * 0.12 * sin(ang);
+    let x2 = ring.x + ring.r * 0.88 * cos(ang);
+    let y2 = ring.y + ring.r * 0.88 * sin(ang);
+    line(x1, y1, x2, y2);
+  }
+
+    // 中环
+  strokeWeight(max(2, ring.r * 0.04));
+  stroke(ring.palette[2]);
+  noFill();
+  circle(ring.x, ring.y, ring.r * 1.2);
+
+  // 点阵圈
+  noStroke();
+  fill(ring.palette[3]);
+  let dots = int(ring.r / 4);
+  for (let i = 0; i < dots; i++){
+    let a = i * TWO_PI / dots;
+    let x = ring.x + ring.r * 0.38 * cos(a);
+    let y = ring.y + ring.r * 0.38 * sin(a);
+    circle(x, y, 6);
+  }
+
+  // 中心帽
+  noStroke();
+  fill(ring.palette[4]);
+  circle(ring.x, ring.y, ring.r * 0.24);
+  fill(bg);
+  circle(ring.x, ring.y, ring.r * 0.12);
 }
